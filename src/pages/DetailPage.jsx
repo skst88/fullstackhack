f56f5362cbfd6781987d9ea7677196d7f5d77f24
Dayana Dzhanuzakova <Dayana@Dayanas-MacBook.local>
2022-01-12 01:47:03 +0600

    smth fix

--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -2,6 +2,9 @@ import React, { useContext, useEffect } from "react";
 import { useParams } from "react-router";
 import { cardsContext } from "../contexts/cardsContext";
 import Comment from "../components/main-page/Comments/Comment";
+import "./DetailPage.css";
+import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const DetailPage = () => {
   const { getProductsToEdit, cardToEdit } = useContext(cardsContext);
@@ -13,40 +16,47 @@ const DetailPage = () => {
   let user = JSON.parse(localStorage.getItem("user"));
 
   return (
-    <div className="container">
-      {cardToEdit ? (
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "space-evenly",
-            marginTop: "20px",
-          }}
-        >
-          <div>
-            <img width="500px" src={cardToEdit.image} alt="product" />
+    <div className="detail-page-bla">
+      <div className="container">
+        {cardToEdit ? (
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "space-evenly",
+              paddingTop: "25px",
+            }}
+          >
+            <div>
+              <img width="500px" src={cardToEdit.image} alt="product" />
+            </div>
+            <div>
+              <h3> Name of card: {cardToEdit.title}</h3>
+              <h3> Description of card: {cardToEdit.description}</h3>
+              <h3> Price: {cardToEdit.price}$</h3>
+              <Link to="/">
+                <Button style={{ backgroundColor: "#b48d62", border: "none" }}>
+                  Back
+                </Button>
+              </Link>
+              <div
+                style={{
+                  marginBottom: "-10px",
+                  width: "30px",
+                  borderRadius: "50%",
+                  height: "30px",
+                  display: "inline-block",
+                }}
+              ></div>
+              <br />
+              <br />
+            </div>
           </div>
-          <div>
-            <h3> Name of card: {cardToEdit.title}</h3>
-            <h3> Description of card: {cardToEdit.description}</h3>
-            <h3> Price: {cardToEdit.price}$</h3>
-            <div
-              style={{
-                marginBottom: "-10px",
-                width: "30px",
-                borderRadius: "50%",
-                height: "30px",
-                display: "inline-block",
-              }}
-            ></div>
-            <br />
-            <br />
-          </div>
-        </div>
-      ) : (
-        <h2>Loading...</h2>
-      )}
+        ) : (
+          <h2>Loading...</h2>
+        )}
 
-      <Comment />
+        <Comment />
+      </div>
     </div>
   );
 };
--- src/pages/EditPage.jsx
+++ src/pages/EditPage.jsx
@@ -22,122 +22,126 @@ const EditPage = () => {
   });
   const navigate = useNavigate();
   return (
-    <div>
-      <h2 className="editproducts-page">Edit page</h2>
-      {cardToEdit ? (
-        <Formik
-          validationSchema={schema}
-          onSubmit={(data, { resetForm }) => {
-            console.log(data);
-            saveEditedProducts(data);
-            navigate(-1);
-          }}
-          initialValues={cardToEdit}
-        >
-          {({ handleSubmit, handleChange, values, touched, errors }) => (
-            <Form
-              style={{ width: "90%", margin: "0 auto" }}
-              className="bg-light p-4"
-              onSubmit={handleSubmit}
-            >
-              <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Name of card</Form.Label>
-                <Form.Control
-                  type="text"
-                  placeholder="Name of card"
-                  name="title"
-                  onChange={handleChange}
-                  isValid={!errors.title && touched.title}
-                  isInvalid={!!errors.title}
-                  value={values.title}
-                />
-                <Form.Control.Feedback type="invalid">
-                  {errors.title}
-                </Form.Control.Feedback>
-              </Form.Group>
-
-              <Form.Group className="mb-3" controlId="formBasicEmail1">
-                <Form.Label>Category of card</Form.Label>
-                <Form.Select
-                  aria-label="Default select example"
-                  name="tag"
-                  onChange={handleChange}
-                  isValid={!errors.tag && touched.tag}
-                  isInvalid={!!errors.tag}
-                  value={values.tag}
-                >
-                  <option value="Greeting card">Greeting card</option>
-                  <option value="Notebook">Notebook</option>
-                  <option value="Assorted card sets">Assorted card sets</option>
-                  <option value="Wallpaper">Wallpaper</option>
-                </Form.Select>
-
-                <Form.Control.Feedback type="invalid">
-                  {errors.tag}
-                </Form.Control.Feedback>
-              </Form.Group>
-              <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Description of card</Form.Label>
-                <Form.Control
-                  type="text"
-                  placeholder="Description of card"
-                  name="description"
-                  onChange={handleChange}
-                  isValid={!errors.description && touched.description}
-                  isInvalid={!!errors.description}
-                  value={values.description}
-                />
-                <Form.Control.Feedback type="invalid">
-                  {errors.description}
-                </Form.Control.Feedback>
-              </Form.Group>
-              <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Image</Form.Label>
-                <Form.Control
-                  type="text"
-                  placeholder="Image"
-                  name="image"
-                  onChange={handleChange}
-                  isValid={!errors.image && touched.image}
-                  isInvalid={!!errors.image}
-                  value={values.image}
-                />
-                <Form.Control.Feedback type="invalid">
-                  {errors.image}
-                </Form.Control.Feedback>
-              </Form.Group>
-              <Form.Group className="mb-3" controlId="formBasicEmail2">
-                <Form.Label>Price</Form.Label>
-                <Form.Control
-                  type="text"
-                  placeholder="Price"
-                  name="price"
-                  onChange={handleChange}
-                  isValid={!errors.price && touched.price}
-                  isInvalid={!!errors.price}
-                  value={values.price}
-                />
-                <Form.Control.Feedback type="invalid">
-                  {errors.price}
-                </Form.Control.Feedback>
-              </Form.Group>
-              <Button
-                style={{
-                  border: "none",
-                  marginLeft: "0",
-                  backgroundColor: "#1C374C",
-                }}
-                variant="primary"
-                type="submit"
+    <div className="editproducts-page">
+      <div>
+        <h2 className="editt">Edit page</h2>
+        {cardToEdit ? (
+          <Formik
+            validationSchema={schema}
+            onSubmit={(data, { resetForm }) => {
+              console.log(data);
+              saveEditedProducts(data);
+              navigate(-1);
+            }}
+            initialValues={cardToEdit}
+          >
+            {({ handleSubmit, handleChange, values, touched, errors }) => (
+              <Form
+                style={{ width: "90%", margin: "0 auto" }}
+                className="bg-light p-4"
+                onSubmit={handleSubmit}
               >
-                Send
-              </Button>
-            </Form>
-          )}
-        </Formik>
-      ) : (
-        <h2>Loading...</h2>
-      )}
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                  <Form.Label>Name of card</Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder="Name of card"
+                    name="title"
+                    onChange={handleChange}
+                    isValid={!errors.title && touched.title}
+                    isInvalid={!!errors.title}
+                    value={values.title}
+                  />
+                  <Form.Control.Feedback type="invalid">
+                    {errors.title}
+                  </Form.Control.Feedback>
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicEmail1">
+                  <Form.Label>Category of card</Form.Label>
+                  <Form.Select
+                    aria-label="Default select example"
+                    name="tag"
+                    onChange={handleChange}
+                    isValid={!errors.tag && touched.tag}
+                    isInvalid={!!errors.tag}
+                    value={values.tag}
+                  >
+                    <option value="Greeting card">Greeting card</option>
+                    <option value="Notebook">Notebook</option>
+                    <option value="Assorted card sets">
+                      Assorted card sets
+                    </option>
+                    <option value="Wallpaper">Wallpaper</option>
+                  </Form.Select>
+
+                  <Form.Control.Feedback type="invalid">
+                    {errors.tag}
+                  </Form.Control.Feedback>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                  <Form.Label>Description of card</Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder="Description of card"
+                    name="description"
+                    onChange={handleChange}
+                    isValid={!errors.description && touched.description}
+                    isInvalid={!!errors.description}
+                    value={values.description}
+                  />
+                  <Form.Control.Feedback type="invalid">
+                    {errors.description}
+                  </Form.Control.Feedback>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                  <Form.Label>Image</Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder="Image"
+                    name="image"
+                    onChange={handleChange}
+                    isValid={!errors.image && touched.image}
+                    isInvalid={!!errors.image}
+                    value={values.image}
+                  />
+                  <Form.Control.Feedback type="invalid">
+                    {errors.image}
+                  </Form.Control.Feedback>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicEmail2">
+                  <Form.Label>Price</Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder="Price"
+                    name="price"
+                    onChange={handleChange}
+                    isValid={!errors.price && touched.price}
+                    isInvalid={!!errors.price}
+                    value={values.price}
+                  />
+                  <Form.Control.Feedback type="invalid">
+                    {errors.price}
+                  </Form.Control.Feedback>
+                </Form.Group>
+                <Button
+                  style={{
+                    border: "none",
+                    marginLeft: "0",
+                    backgroundColor: "#b48d62",
+                  }}
+                  variant="primary"
+                  type="submit"
+                >
+                  Send
+                </Button>
+              </Form>
+            )}
+          </Formik>
+        ) : (
+          <h2>Loading...</h2>
+        )}
+      </div>
     </div>
   );
 };
